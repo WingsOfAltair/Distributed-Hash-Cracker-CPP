3b5bd186df02d129bd32c343e87c064998163b55
Bashar M. Bataineh <basharbataineh@hotmail.com>
2025-07-02 20:28:02 +0300

    Updated mutation list generator.

--- x64/Debug/mutation_list_generator_upto_n_times.py
+++ x64/Debug/mutation_list_generator_upto_n_times.py
@@ -4,8 +4,25 @@ chars = ['l', 'u', 'r', 'c', 't', 'd', 's', 'n', '3']
 max_len = 6
 start_writing = False
 
-with open("rule_combinations_no_lu_from_l_up_to_3.txt", "w", encoding="utf-8") as f:
+with open("mutation_list.txt", "w", encoding="utf-8") as f:
     first = True
+    
+    f.write("# If mutator rules list is empty, mutation are disabled.\n")
+    f.write("# However, if it is enabled, it will attempt to mutate\n")
+    f.write("# each word depending on the rule.\n")
+    f.write("\n")
+    f.write("# l = lowercase\n")
+    f.write("# u = uppercase\n")
+    f.write("# r = reverse\n")
+    f.write("# c = capitalize first letter\n")
+    f.write("# t = toggle case\n")
+    f.write("# d = duplicate\n")
+    f.write("# s = substitute sXY\n")
+    f.write("# n = append numbers\n")
+    f.write("# 3 = l33tsp3@k\n")
+    f.write("\n")
+    f.write("MUTATION_RULES=")
+    
     for length in range(1, max_len + 1):
         for combo in product(chars, repeat=length):
             combo_str = ''.join(combo)
